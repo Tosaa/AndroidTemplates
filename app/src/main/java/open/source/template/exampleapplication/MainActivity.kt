18f91d985368ab31b76587c1008b919d81e1c2b6
Anton Saatze <a.saatze@googlemail.com>
2021-02-26 18:54:21 +0100

    add data-binding

--- app/src/main/java/open/source/template/exampleapplication/MainActivity.kt
+++ app/src/main/java/open/source/template/exampleapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package open.source.template.exampleapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import open.source.template.exampleapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main)
+        binding.text = "Example Project"
     }
 }
